24d13a28585d7ddda416c6456b86639fd507637c
acaibawl <hagiya0719@gmail.com>
2025-05-11 01:01:27 +0900

    pintによる既存コードの整形。declare(strict_types=1);を強制

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\HealthCheckController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', [HealthCheckController::class, 'index'])->name('health');;
-
+Route::get('/health', [HealthCheckController::class, 'index'])->name('health');
