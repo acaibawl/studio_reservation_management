55589c12a3c7ca73e905697088c4c8c6f721a398
acaibawl <hagiya0719@gmail.com>
2025-05-11 23:41:09 +0900

    healthチェックのテスト作成。analyseのworkflow名を修正

---
+++ backend/tests/Feature/App/Http/Controllers/HealthCheckControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Feature\App\Http\Controllers;
+
+use Tests\TestCase;
+
+class HealthCheckControllerTest extends TestCase
+{
+    /**
+     * healthチェックのルートテスト
+     */
+    public function test_index(): void
+    {
+        $response = $this->get('/health');
+
+        $response->assertStatus(200);
+    }
+}
