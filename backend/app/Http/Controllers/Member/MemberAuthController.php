2d98fc8a303e28bb21b772e5226d842fc563d0bf
acaibawl <hagiya0719@gmail.com>
2025-05-20 13:38:43 +0900

    メール送信方法をSMTPに変更し、Mailpitコンテナを追加。サンプルのメール送信コードを追加

--- backend/app/Http/Controllers/Member/MemberAuthController.php
+++ backend/app/Http/Controllers/Member/MemberAuthController.php
@@ -13,12 +13,16 @@ class MemberAuthController extends Controller
 {
     public function sendRegisterAuthenticationCode(): JsonResponse
     {
+        \Mail::raw('test from laravel 本文です。', function($message) {
+            $message->to('atesaki@example.com')->subject('testメールタイトルです。');
+        });
+
         /** @var PhpRedisConnection $connection */
         $connection = Redis::connection();
         $connection->set('aaa', '123456');
 
         return response()->json([
-            'message' => '会員登認証コードを送信しました。',
+            'message' => '会員登録認証コードを送信しました。',
         ]);
     }
 }
