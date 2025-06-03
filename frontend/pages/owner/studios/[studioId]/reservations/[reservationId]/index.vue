fe9227f0c49e0db53db79727e627a1b5dd86c78d
acaibawl <hagiya0719@gmail.com>
2025-06-03 12:19:44 +0900

    予約詳細ページで会員IDをクリック可能にし、該当会員ページへのリンクを追加

--- frontend/pages/owner/studios/[studioId]/reservations/[reservationId]/index.vue
+++ frontend/pages/owner/studios/[studioId]/reservations/[reservationId]/index.vue
@@ -64,17 +64,19 @@ const handleDeleteClick = async () => {
     <p class="text-body-1 mt-5">利用時間</p>
     <p class="text-body-1 ml-5">{{ reservation.usageHour }}時間</p>
     <p class="text-body-1 mt-5">会員ID</p>
-    <p class="text-body-1 ml-5">{{ reservation.memberId }}</p>
+    <p class="text-body-1 ml-5"><NuxtLink :to="`/owner/members/${reservation.memberId}`" class="text-primary">{{ reservation.memberId }}</NuxtLink></p>
     <p class="text-body-1 mt-5">{{ reservation.memberName }}様</p>
     <p class="text-body-1 mt-5">メモ</p>
     <p class="text-body-1 ml-5">{{ reservation.memo }}</p>
     <v-row class="mt-5">
-      <v-btn :to="`/owner/reservations/date/${reservation.startAtDateToYYYYMMDDKebab}`">戻る</v-btn>
+      <v-btn :to="`/owner/reservations/date/${reservation.startAtDateToYYYYMMDDKebab}`">この日の予約一覧へ</v-btn>
       <v-btn :to="`/owner/studios/${studioId}/reservations/${reservationId}/edit`" class="ml-5" color="primary">修正</v-btn>
     </v-row>
   </div>
 </template>
 
 <style scoped>
-
+a {
+  text-decoration: none;
+}
 </style>
