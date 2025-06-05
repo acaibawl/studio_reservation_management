fef7c07b0b493f8c4d2c5c326bf8e143a78a7309
acaibawl <hagiya0719@gmail.com>
2025-06-06 00:20:12 +0900

    エラーページにアイコンを追加し、トップページへのリンクにスタイルを適用

--- frontend/error.vue
+++ frontend/error.vue
@@ -27,9 +27,9 @@ if (authOwnerStore.isLogin) {
     <NuxtLayout :name="layout">
 
       <v-sheet class="mt-10 mx-auto pa-5" max-width="600">
-        <p class="mt-5">ステータスコード：{{ error?.statusCode }}</p>
+        <p class="mt-5"><v-icon icon="mdi-alert-circle" class="mr-3" color="red-darken-4"/>ステータスコード：{{ error?.statusCode }}<v-icon icon="mdi-alert-circle" class="ml-3" color="red-darken-4"/></p>
         <p class="mt-5">{{ error?.message }}</p>
-        <p  class="mt-5"><NuxtLink :to="topPageUrl">トップページへ戻る</NuxtLink></p>
+        <p  class="mt-5"><NuxtLink class="text-decoration-none text-primary" :to="topPageUrl">トップページへ戻る</NuxtLink></p>
       </v-sheet>
 
       <loading-overlay />
